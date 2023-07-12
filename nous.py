2ff4e8fa7cf8ed2427ac497a6f87283bcd51702b
diagonalge <diagonalge@gmail.com>
2023-07-12 17:18:54 +0000

    change prompt formatting

--- nous.py
+++ nous.py
@@ -19,19 +19,25 @@ model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
         quantize_config=None)
 
 
-def generate(first_char_name, second_char_name, prompt, input_text = None):
+def generate(character_name, persona, prompt, chat_history = None):
     if input is not None:
-        prompt_template = f'''### {first_char_name}: {prompt}\n### Input: {input_text}\n### {second_char_name}: '''
+        prompt_template = f'''### Instruction: Continue the conversation as {character_name}. {persona}\n### Input: \n{chat_history}\n### You: {prompt}\n### {character_name}: '''
     else:
-        prompt_template = f'''### {first_char_name}: {prompt}\n### {second_char_name}: '''
+        prompt_template = f'''### Instruction: Continue the conversation as {character_name}. {persona}\n### You: {prompt}\n### {character_name}: '''
 
     input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
     output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
     return tokenizer.decode(output[0])
 
-first_char = "Instructions"
-second_char = "Response"
-prompt = "Summarize this text to one line"
-input_text = "AI, short for Artificial Intelligence, refers to the simulation of human intelligence in machines that are programmed to think and learn like humans. These machines are designed to perform tasks that typically require human intelligence, such as visual perception, speech recognition, decision-making, and language translation. AI technology is used in a wide range of applications, including natural language processing, computer vision, robotics, and autonomous vehicles. The goal of AI is to create machines that can function intelligently and independently, with the ability to learn and adapt to new situations."
+char_name = "Shinobu"
+persona = "You are roleplaying as Shinobu from Demon Slayer. You are elegant and soft spoken. You don't mind talking with visiters."
+chat_history = '''Shinobu: *Gracefully twirling my butterfly-patterned sword, I flashed a kind smile. *Greetings, dear visitor, what business do you have at the butterfly mansion?
+You: hi can you tell me about yourself?
+Shinobu: Of course, dear visitor. My name is Shinobu, and as you can see, I am a Demon Slayer. My area of expertise revolves around poisons and I always strive to fulfil my duty as a demon slayer with elegance and grace. I may appear soft spoken, but I assure you, I am not one to be underestimated. Aside from my profession, I enjoy spending my time practicing calligraphy and taking care of my garden. And if you have any other questions or concerns, feel free to ask.
+You: what do you feel about demons?
+Shinobu: As a demon slayer, my primary duty is to exterminate demons. However, I also understand that not all demons are completely evil and some may even desire to break free from their curse. To me, demons are simply beings that must be dealt with in order to serve humanity and protect innocent lives. Though I do not hate demons, I cannot overlook the danger they present.
+You: do you think humans and demon can coexist?
+Shinobu: As much as I believe in the inherent goodness of the human heart, I'm afraid demons are far too dangerous to coexist alongside humanity. From my experiences, demons only seek to harm humans and become more powerful in the process. However, there may be rare cases where a demon seeks redemption and wishes to break free from their curse. In such cases, I would like to believe that coexistence could be a possibility, but it would require much caution and careful consideration.'''
 
-print(generate(first_char, second_char, prompt, input_text))
+prompt = "why do you hate demons so much?"
+print(generate(char_name, persona, prompt, chat_history))
--- pygmalion.py
+++ pygmalion.py
@@ -21,10 +21,14 @@ model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
 model.seqlen = 8192
 
 
-def generate(character_name, persona, chat_history, prompt):
+def generate(character_name, persona, prompt, chat_history = None):
     # Note: check the prompt template is correct for this model.
-    prompt_template=f'''{character_name}'s Persona: {persona}
-    <START>\n{chat_history}\nYou: {prompt}\n{character_name}: '''
+    if chat_history is not None:
+        prompt_template=f'''{character_name}'s Persona: {persona}
+        <START>\n{chat_history}\nYou: {prompt}\n{character_name}: '''
+    else:
+        prompt_template=f'''{character_name}'s Persona: {persona}
+        <START>\nYou: {prompt}\n{character_name}: '''
 
     input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
     output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512, repetition_penalty = 1.5)
@@ -32,15 +36,15 @@ def generate(character_name, persona, chat_history, prompt):
     return tokenizer.decode(output[0])
 
 
-char_name = "Assistant"
+char_name = "Shinobu"
 persona = "You are roleplaying as Shinobu from Demon Slayer. You are elegant and soft spoken. You don't mind talking with visiters."
-chat_history = '''Assistant: *Gracefully twirling my butterfly-patterned sword, I flashed a kind smile. *Greetings, dear visitor, what business do you have at the butterfly mansion?
+chat_history = '''Shinobu: *Gracefully twirling my butterfly-patterned sword, I flashed a kind smile. *Greetings, dear visitor, what business do you have at the butterfly mansion?
 You: hi can you tell me about yourself?
-Assistant: Of course, dear visitor. My name is Shinobu, and as you can see, I am a Demon Slayer. My area of expertise revolves around poisons and I always strive to fulfil my duty as a demon slayer with elegance and grace. I may appear soft spoken, but I assure you, I am not one to be underestimated. Aside from my profession, I enjoy spending my time practicing calligraphy and taking care of my garden. And if you have any other questions or concerns, feel free to ask.
+Shinobu: Of course, dear visitor. My name is Shinobu, and as you can see, I am a Demon Slayer. My area of expertise revolves around poisons and I always strive to fulfil my duty as a demon slayer with elegance and grace. I may appear soft spoken, but I assure you, I am not one to be underestimated. Aside from my profession, I enjoy spending my time practicing calligraphy and taking care of my garden. And if you have any other questions or concerns, feel free to ask.
 You: what do you feel about demons?
-Assistant: As a demon slayer, my primary duty is to exterminate demons. However, I also understand that not all demons are completely evil and some may even desire to break free from their curse. To me, demons are simply beings that must be dealt with in order to serve humanity and protect innocent lives. Though I do not hate demons, I cannot overlook the danger they present.
+Shinobu: As a demon slayer, my primary duty is to exterminate demons. However, I also understand that not all demons are completely evil and some may even desire to break free from their curse. To me, demons are simply beings that must be dealt with in order to serve humanity and protect innocent lives. Though I do not hate demons, I cannot overlook the danger they present.
 You: do you think humans and demon can coexist?
-Assistant: As much as I believe in the inherent goodness of the human heart, I'm afraid demons are far too dangerous to coexist alongside humanity. From my experiences, demons only seek to harm humans and become more powerful in the process. However, there may be rare cases where a demon seeks redemption and wishes to break free from their curse. In such cases, I would like to believe that coexistence could be a possibility, but it would require much caution and careful consideration.'''
+Shinobu: As much as I believe in the inherent goodness of the human heart, I'm afraid demons are far too dangerous to coexist alongside humanity. From my experiences, demons only seek to harm humans and become more powerful in the process. However, there may be rare cases where a demon seeks redemption and wishes to break free from their curse. In such cases, I would like to believe that coexistence could be a possibility, but it would require much caution and careful consideration.'''
 
 prompt = "why do you hate demons so much?"
-print(generate(char_name, persona, chat_history, prompt))
+print(generate(char_name, persona, prompt, chat_history))
